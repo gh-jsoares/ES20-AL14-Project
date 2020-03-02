a81ee4efea8a56a73f93fe06d1285c9bb0e573c0
DFL13 <dani.f.lopes@hotmail.com>
2020-03-02 13:14:17 +0000

    #iss6 Defined tests

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentService.java
@@ -0,0 +1,7 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.tournament;
+
+public class TournamentService {
+    public void TournamentService(){
+
+    }
+}
---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentEnrollSpockTest.groovy
@@ -0,0 +1,37 @@
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.TournamentService
+import spock.lang.Specification
+
+class TournamentEnrollSpockTest extends Specification{
+
+    def tournService
+
+    def setup() {
+        tournService = new TournamentService()
+    }
+
+    def "the tournament both exists and is open and create tournament enroll"() {
+        //the tournament enroll is created for the username
+        expect: false
+    }
+
+    def "the tournament exists but is not open"() {
+        //an exception is thrown
+        expect: false
+    }
+
+    def "the tournament and tournament enroll both exist for the username"() {
+        //an exception is thrown
+        expect: false
+    }
+
+    def "the tournament does not exist"() {
+        //an exception is thrown
+        expect: false
+    }
+
+    def "the username does not exist"() {
+        //an exception is thrown
+        expect: false
+    }
+
+}
