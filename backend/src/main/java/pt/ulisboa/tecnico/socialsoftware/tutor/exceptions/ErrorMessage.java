757b5083be3a85d25696e886b533bd6f9419f9bc
jm <josemiguelg@outlook.pt>
2020-03-12 17:08:50 -0700

    student visualizes teacher answers, closes #65

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/exceptions/ErrorMessage.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/exceptions/ErrorMessage.java
@@ -63,6 +63,7 @@ public enum ErrorMessage {
     AUTHENTICATION_ERROR("Authentication Error"),
     FENIX_CONFIGURATION_ERROR("Incorrect server configuration files for fenix"),
     USER_IS_NOT_TEACHER("User with name %s is not a teacher"),
+    USER_IS_NOT_STUDENT("User with name %s is not a student"),
 
 
     ACCESS_DENIED("You do not have permission to view this resource"),
