8d4a913111f70011beb344193aa57ec317170b82
alexduarte23 <alex.aires.duarte@gmail.com>
2020-03-16 10:56:10 -0700

    issue #106 - added controller method to create tournament

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentController.java
@@ -1,20 +1,14 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.tournament;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseDto;
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException;
-import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto;
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.User;
 
 import javax.validation.Valid;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.security.Principal;
-import java.util.List;
 
 import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.AUTHENTICATION_ERROR;
 
@@ -36,4 +30,16 @@ public class TournamentController {
         return tournamentService.tournamentEnrollStudent(tournamentId, user.getId());
     }
 
+
+    @PostMapping("/executions/{executionId}/tournaments")
+    @PreAuthorize("(hasRole('ROLE_STUDENT') or hasRole('ROLE_DEMO_STUDENT')) and hasPermission(#executionId, 'EXECUTION.ACCESS')")
+    public TournamentDto createTournament(Principal principal, @PathVariable int executionId, @Valid @RequestBody TournamentDto tournDto) {
+        User user = (User) ((Authentication) principal).getPrincipal();
+
+        if(user == null){
+            throw new TutorException(AUTHENTICATION_ERROR);
+        }
+
+        return tournamentService.createTournament(executionId, tournDto, user.getId());
+    }
 }
