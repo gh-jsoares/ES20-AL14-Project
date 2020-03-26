46b24560d89c2b6864b64c20b0ea459c3c38c48b
João Soares <jsoares.public@gmail.com>
2020-03-26 10:07:11 +0000

    Added profiler test for createStudentQUestion service. Close #140

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/studentquestions/CreateStudentQuestionPerformanceSpockTest.groovy
@@ -0,0 +1,98 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.question.service.studentquestions
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.Course
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.StudentQuestionService
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.StudentQuestion
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.OptionDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.StudentQuestionDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.StudentQuestionRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.UserRepository
+import spock.lang.Specification
+
+@DataJpaTest
+class CreateStudentQuestionPerformanceSpockTest extends Specification {
+
+    public static final String COURSE_NAME = "Software Architecture"
+    public static final String USER_NAME = "Alfredo Costa"
+    public static final String USER_USERNAME = "alcosta"
+    public static final String QUESTION_TITLE = 'question title'
+    public static final String QUESTION_CONTENT = 'question content'
+    public static final String OPTION_CONTENT = "optionId content"
+
+    @Autowired
+    StudentQuestionService studentQuestionService
+
+    @Autowired
+    UserRepository userRepository
+
+    @Autowired
+    CourseRepository courseRepository
+
+    @Autowired
+    StudentQuestionRepository studentQuestionRepository
+
+    User user
+    Course course
+
+    def setup() {
+        user = new User(USER_NAME, USER_USERNAME, 1, User.Role.STUDENT)
+        userRepository.save(user)
+
+        course = new Course(COURSE_NAME, Course.Type.TECNICO)
+        courseRepository.save(course)
+    }
+
+    def "create 10000 student question"() {
+        given: "10000 studentquestionDTO"
+        StudentQuestionDto[] studentQuestionDto = []
+
+        0.upto(10000, {
+            def sq = createStudentQuestionDto(QUESTION_TITLE + it, QUESTION_CONTENT, StudentQuestion.Status.AWAITING_APPROVAL.name())
+            createOptions(sq, OPTION_CONTENT, 4, 1)
+            studentQuestionDto += sq
+        })
+
+        when:
+        0.upto(10000, { studentQuestionService.createStudentQuestion(course.getId(), user.getId(), studentQuestionDto[it.intValue()]) })
+
+        then:
+        true
+    }
+
+    private static createOptions(StudentQuestionDto studentQuestionDto, String content, int number_of_options, int number_of_correct) {
+        def options = new HashSet<OptionDto>()
+        for (int i = 0; i < number_of_options; i++) {
+            def optionDto = new OptionDto()
+            optionDto.setContent(content)
+
+            if (number_of_correct-- > 0)
+                optionDto.setCorrect(true)
+
+            options.add(optionDto)
+        }
+        studentQuestionDto.setOptions(options)
+    }
+
+    private static StudentQuestionDto createStudentQuestionDto(String title, String content, String status) {
+        def studentQuestionDto = new StudentQuestionDto()
+        studentQuestionDto.setKey(1)
+        studentQuestionDto.setTitle(title)
+        studentQuestionDto.setContent(content)
+        studentQuestionDto.setStatus(status)
+        studentQuestionDto
+    }
+
+    @TestConfiguration
+    static class QuestionServiceImplTestContextConfiguration {
+        @Bean
+        StudentQuestionService studentQuestionService() {
+            return new StudentQuestionService()
+        }
+    }
+}
