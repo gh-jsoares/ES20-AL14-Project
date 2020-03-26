f951c73602ce5f4a683567fad2c971171a735673
João Soares <jsoares.public@gmail.com>
2020-03-26 18:41:42 +0000

    Added load test for update topics in student question. Close #147

--- backend/jmeter/studentquestion/gencsv.py
+++ backend/jmeter/studentquestion/gencsv.py
@@ -1,15 +1,21 @@
+from os import path
+
 AMOUNT = 10000
+FILE_NAME = "StudentQuestionData.csv"
 
 count = 0
-with open("StudentQuestionData.csv", "r") as f:
-    print("Counting lines from file...")
-    for i in f:
-        count += 1
-        
+if path.exists(FILE_NAME):
+    with open(FILE_NAME, "r") as f:
+        print("Counting lines from file...")
+        for i in f:
+            count += 1
+
+
 print("Initial line count: {}".format(count))
-with open("StudentQuestionData.csv", "a+") as f:
+with open(FILE_NAME, "a+") as f:
     print("Appending lines to file...")
     for i in range(AMOUNT):
-        f.write("Demo Question {}\n".format(i+count+1))
+        id = i+count+1
+        f.write("{},Demo Question {}\n".format(id, id))
 
 print("Final line count: {}".format(count+AMOUNT))
