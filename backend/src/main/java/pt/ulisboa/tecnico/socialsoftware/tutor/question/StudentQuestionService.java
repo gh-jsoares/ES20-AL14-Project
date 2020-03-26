85ef281a66f6ce7649596f0c8724896fcbe69efc
João Soares <jsoares.public@gmail.com>
2020-03-26 18:20:58 +0000

    Added Update Student QUestions Topics functionality test. Close #146

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/StudentQuestionService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/StudentQuestionService.java
@@ -201,6 +201,7 @@ public class StudentQuestionService {
     @Retryable(
             value = { SQLException.class },
             backoff = @Backoff(delay = 5000))
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
     public boolean canAccessStudentQuestion(int userId, int studentQuestionId) {
         User user = getUserIfExists(userId);
         StudentQuestion studentQuestion = getStudentQuestionIfExists(studentQuestionId);
