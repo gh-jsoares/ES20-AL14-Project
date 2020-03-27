9ea112c2d5bf5ed5b62a8a280d79d2d7fe1147a4
João Soares <jsoares.public@gmail.com>
2020-03-27 14:05:17 +0000

    Ran all student questions jmeter tests again (to take screenshots)

---
+++ backend/jmeter/studentquestion/_genfolders.py
@@ -0,0 +1,14 @@
+from os import walk, makedirs, path
+
+for r, d, fs in walk('.'):
+    if r != '.':
+        break
+    for f in fs:
+        s = f.split('.')
+        name = s[0]
+        ext = s[1]
+        if ext == 'jmx':
+            dirpath = "pictures/{}".format(name)
+            if not path.exists(dirpath):
+                makedirs(dirpath)
+            print("R:{}, D:{}, F:{}".format(r, d, name))
