8e006ac9a23c1529bdae3efb08924532a9200e70
João Soares <jsoares.public@gmail.com>
2020-04-18 11:51:48 +0100

    Setup routing and page for listing questions. Students can list topics now. Close #229

--- frontend/src/views/student/questions/StudentQuestionsView.vue
+++ frontend/src/views/student/questions/StudentQuestionsView.vue
@@ -1,12 +1,16 @@
 <template>
-  <h1>Hello</h1>
+  <v-card class="table">
+    <h1>StudentQuestions</h1>
+  </v-card>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Watch } from 'vue-property-decorator';
+import RemoteServices from '@/services/RemoteServices';
+import StudentQuestion from '@/models/management/StudentQuestion';
 
 @Component
-export default class QuestionsView extends Vue {}
+export default class StudentQuestionsView extends Vue {}
 </script>
 
 <style lang="scss" scoped></style>
