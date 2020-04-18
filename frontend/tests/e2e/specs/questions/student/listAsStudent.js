cb0812104b2acc7a631bf957dc152f2aa61a8c16
João Soares <jsoares.public@gmail.com>
2020-04-18 21:38:05 +0100

    Using fixtures now, looks nicer

--- frontend/tests/e2e/specs/questions/student/listAsStudent.js
+++ frontend/tests/e2e/specs/questions/student/listAsStudent.js
@@ -1,27 +1,33 @@
 describe('List Student Questions as Student walkthrough', () => {
-    beforeEach(() => {
-      cy.demoStudentLogin();
-      cy.queryDatabase(`INSERT INTO student_questions (id, key, title, content, course_id, student_id, status, creation_date) VALUES ('10000','10000', 'Student Question Title 1', 'Student Question Content', '2', '676', 'AWAITING_APPROVAL', NOW());`);
-      cy.queryDatabase(`INSERT INTO student_questions (id, key, title, content, course_id, student_id, status, creation_date) VALUES ('20000','20000', 'Student Question Title 2', 'Student Question Content', '2', '676', 'AWAITING_APPROVAL', NOW());`);
-      cy.wait(500);
-      cy.gotoStudentQuestions();
+  beforeEach(() => {
+    cy.demoStudentLogin();
+    
+    cy.fixture('questions/student/studentQuestionsData.json').then(data => {
+      data.student_questions.forEach(studentQuestion => {
+        cy.queryDatabase(`INSERT INTO student_questions (id, key, title, content, course_id, student_id, status, creation_date) VALUES ('${studentQuestion.id}', '${studentQuestion.id}', '${studentQuestion.title} ${studentQuestion.id}', '${studentQuestion.content}', '${studentQuestion.course_id}', '${studentQuestion.student_id}', '${studentQuestion.status}', ${studentQuestion.creation_date});`);
+      });
     });
-  
-    afterEach(() => {
-      cy.contains('Logout').click();
-      cy.queryDatabase(`DELETE FROM student_questions where id = '10000';`);
-      cy.queryDatabase(`DELETE FROM student_questions where id = '20000';`);
+
+    cy.gotoStudentQuestions();
+  });
+
+  afterEach(() => {
+    cy.contains('Logout').click();
+    cy.fixture('questions/student/studentQuestionsData.json').then(data => {
+      data.student_questions.forEach(studentQuestion => {
+        cy.queryDatabase(`DELETE FROM student_questions where id = '${studentQuestion.id}';`);
+      });
     });
-  
-    it('login list student questions', () => {
-      cy.contains('Student Question Title')
-        .parent()
-        .should('have.length', 1)
-        .parent()
-        .children()
-        .should('have.length', 2) // 2 questions
-        .children()
-        .should('have.length', 14) // 7 * 2 questions
-    });
+  });
+
+  it('login list student questions', () => {
+    cy.contains('Student Question Title')
+      .parent()
+      .should('have.length', 1)
+      .parent()
+      .children()
+      .should('have.length', 2) // 2 questions
+      .children()
+      .should('have.length', 14); // 7 * 2 questions
+  });
 });
-  
--- frontend/tests/e2e/specs/questions/student/viewAsStudent.js
+++ frontend/tests/e2e/specs/questions/student/viewAsStudent.js
@@ -1,25 +1,32 @@
 describe('List Student Questions as Student walkthrough', () => {
-    beforeEach(() => {
-      cy.demoStudentLogin();
-      cy.queryDatabase(`INSERT INTO student_questions (id, key, title, content, course_id, student_id, status, creation_date) VALUES ('10000','10000', 'Student Question Title 1', 'Student Question Content', '2', '676', 'AWAITING_APPROVAL', NOW());`);
-      cy.wait(500);
-      cy.gotoStudentQuestions();
+  beforeEach(() => {
+    cy.demoStudentLogin();
+    
+    cy.fixture('questions/student/studentQuestionsData.json').then(data => {
+      const studentQuestion = data.student_questions[0];
+      cy.queryDatabase(`INSERT INTO student_questions (id, key, title, content, course_id, student_id, status, creation_date) VALUES ('${studentQuestion.id}', '${studentQuestion.id}', '${studentQuestion.title} ${studentQuestion.id}', '${studentQuestion.content}', '${studentQuestion.course_id}', '${studentQuestion.student_id}', '${studentQuestion.status}', ${studentQuestion.creation_date});`);
     });
-  
-    afterEach(() => {
-      cy.contains('Logout').click();
-      cy.queryDatabase(`DELETE FROM student_questions where id = '10000';`);
+
+    cy.gotoStudentQuestions();
+  });
+
+  afterEach(() => {
+    cy.contains('Logout').click();
+
+    cy.fixture('questions/student/studentQuestionsData.json').then(data => {
+      const studentQuestion = data.student_questions[0];
+      cy.queryDatabase(`DELETE FROM student_questions where id = '${studentQuestion.id}';`);
     });
-  
-    it('login view student question details', () => {
-      cy.contains('Student Question Title')
-        .parent()
-        .should('have.length', 1)
-        .children()
-        .should('have.length', 7)
-        .find('[data-cy="viewStudentQuestionDetails"]')
-        .click();
-      cy.wait(2000);
-    });
+  });
+
+  it('login view student question details', () => {
+    cy.contains('Student Question Title')
+      .parent()
+      .should('have.length', 1)
+      .children()
+      .should('have.length', 7)
+      .find('[data-cy="viewStudentQuestionDetails"]')
+      .click();
+  });
 });
   
