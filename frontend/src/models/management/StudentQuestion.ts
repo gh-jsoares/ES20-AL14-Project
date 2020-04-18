17b9df69c238552e0a56f1e3d29e7d415624d331
João Soares <jsoares.public@gmail.com>
2020-04-18 12:05:46 +0100

    Fixed frontend student question model. Fix #229. Starting with H3

--- frontend/src/models/management/StudentQuestion.ts
+++ frontend/src/models/management/StudentQuestion.ts
@@ -6,15 +6,14 @@ export default class StudentQuestion {
   id: number | null = null;
   title: string = '';
   status: string = 'AVAILABLE';
-  numberOfAnswers!: number;
-  numberOfGeneratedQuizzes!: number;
-  numberOfNonGeneratedQuizzes!: number;
-  numberOfCorrect!: number;
-  difficulty!: number | null;
   content: string = '';
   creationDate!: string | null;
   image: Image | null = null;
   sequence: number | null = null;
+  creatorUsername!: number | null;
+  lastReviewerUsername!: number | null;
+  reviewedDate!: string | null;
+  rejectedExplanation!: string | null;
 
   options: Option[] = [new Option(), new Option(), new Option(), new Option()];
   topics: Topic[] = [];
@@ -24,14 +23,14 @@ export default class StudentQuestion {
       this.id = jsonObj.id;
       this.title = jsonObj.title;
       this.status = jsonObj.status;
-      this.numberOfAnswers = jsonObj.numberOfAnswers;
-      this.numberOfGeneratedQuizzes = jsonObj.numberOfGeneratedQuizzes;
-      this.numberOfNonGeneratedQuizzes = jsonObj.numberOfNonGeneratedQuizzes;
-      this.numberOfCorrect = jsonObj.numberOfCorrect;
-      this.difficulty = jsonObj.difficulty;
       this.content = jsonObj.content;
       this.image = jsonObj.image;
       this.creationDate = jsonObj.creationDate;
+
+      this.creatorUsername = jsonObj.creatorUsername;
+      this.lastReviewerUsername = jsonObj.lastReviewerUsername;
+      this.reviewedDate = jsonObj.creationDate;
+      this.rejectedExplanation = jsonObj.creationDate;
 
       this.options = jsonObj.options.map(
         (option: Option) => new Option(option)
