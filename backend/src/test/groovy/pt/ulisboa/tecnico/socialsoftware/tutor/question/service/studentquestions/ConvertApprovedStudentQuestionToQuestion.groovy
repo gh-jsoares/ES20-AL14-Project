f3f3d656ad50b47fdc7b95cd93c50d54e34fe0aa
João Soares <jsoares.public@gmail.com>
2020-05-04 11:03:44 +0100

    Implemented tests. Close #272

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/studentquestions/ConvertApprovedStudentQuestionToQuestion.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/studentquestions/ConvertApprovedStudentQuestionToQuestion.groovy
@@ -1,9 +1,24 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.question.service.studentquestions
 
-
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.Course
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.StudentQuestionService
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.StudentQuestion
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.QuestionRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.StudentQuestionRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.UserRepository
 import spock.lang.Specification
 import spock.lang.Unroll
+
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.STUDENT_QUESTION_NOT_AWAITING_APPROVAL
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.STUDENT_QUESTION_NOT_FOUND
 
 @DataJpaTest
 class ConvertApprovedStudentQuestionToQuestion extends Specification {
@@ -17,12 +32,111 @@ class ConvertApprovedStudentQuestionToQuestion extends Specification {
     public static final String OPTION_CONTENT = "optionId content"
     public static final String COURSE_NAME = "Arquitetura de Software"
 
+    @Autowired
+    StudentQuestionService studentQuestionService
+
+    @Autowired
+    UserRepository userRepository
+
+    @Autowired
+    StudentQuestionRepository studentQuestionRepository
+
+    @Autowired
+    QuestionRepository questionRepository
+
+    @Autowired
+    CourseRepository courseRepository
+
+    User user
+    User teacher
+    StudentQuestion studentQuestion
+    Course course
+
+    def setup() {
+        user = new User(USER_NAME, USER_USERNAME, 1, User.Role.STUDENT)
+        userRepository.save(user)
+
+        teacher = new User(TEACHER_NAME, TEACHER_USERNAME, 2, User.Role.TEACHER)
+        userRepository.save(teacher)
+
+        course = new Course(COURSE_NAME, Course.Type.TECNICO)
+        courseRepository.save(course)
+
+        studentQuestion = createStudentQuestion(QUESTION_TITLE, QUESTION_CONTENT, StudentQuestion.Status.AWAITING_APPROVAL.name())
+    }
+
     def "convert existing student question to question"() {
-        expect: true
+        given: "an existing student question"
+        def studentQuestionId = studentQuestion.getId()
+
+        and: "its awaiting approval"
+        studentQuestion.status = StudentQuestion.Status.AWAITING_APPROVAL
+
+        when:
+        studentQuestionService.approveStudentQuestion(teacher.id, studentQuestionId)
+
+        then: "a question is created"
+        questionRepository.count() == 1L
+        def result = questionRepository.findAll().get(0)
+        result.getId() != null
+        result.getKey() == 1
+        result.getStatus() == Question.Status.AVAILABLE
+        result.getTitle() == QUESTION_TITLE
+        result.getContent() == QUESTION_CONTENT
+        result.getOptions().size() == 4
+        result.getOptions().stream().allMatch({ o -> o.getContent() == OPTION_CONTENT })
+        result.getOptions().stream().filter({ o -> o.getCorrect() }).count() == 1L
+        result.getImage() == null
+        result.getCourse().getId() == course.getId()
     }
 
     @Unroll
     def "invalid data: studentQuestion=#isStudentQuestion | topic=#isAwaitingApproval || errorMessage=#errorMessage"() {
-        expect: true
+        given: "a student question"
+        def studentQuestionId = createStudentQuestion(isStudentQuestion, isAwaitingApproval)
+
+        when:
+        studentQuestionService.updateStudentQuestionTopics(studentQuestionId, [topicId] as Integer[])
+
+        then:
+        def error = thrown(TutorException)
+        error.errorMessage == errorMessage
+
+        where:
+        isStudentQuestion | isAwaitingApproval || errorMessage
+        false             | true               || STUDENT_QUESTION_NOT_FOUND
+        true              | false              || STUDENT_QUESTION_NOT_AWAITING_APPROVAL
+    }
+
+    private int createStudentQuestion(boolean isStudentQuestion, boolean isAwaitingApproval) {
+        if (isStudentQuestion) {
+            if (isAwaitingApproval)
+                studentQuestion.status = StudentQuestion.Status.AWAITING_APPROVAL
+            else
+                studentQuestion.status = StudentQuestion.Status.ACCEPTED
+            return studentQuestion.getId()
+        }
+        return -1
+    }
+
+    private StudentQuestion createStudentQuestion(String title, String content, String status) {
+        def studentQuestion = new StudentQuestion()
+        studentQuestion.setKey(1)
+        studentQuestion.setTitle(title)
+        studentQuestion.setContent(content)
+        studentQuestion.setStatus(StudentQuestion.Status.valueOf(status))
+        studentQuestion.setStudent(user)
+        studentQuestionRepository.save(studentQuestion)
+        studentQuestion.setCourse(course)
+        course.addStudentQuestion(studentQuestion)
+        studentQuestion
+    }
+
+    @TestConfiguration
+    static class QuestionServiceImplTestContextConfiguration {
+        @Bean
+        StudentQuestionService studentQuestionService() {
+            return new StudentQuestionService()
+        }
     }
 }
