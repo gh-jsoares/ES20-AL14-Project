7782642dfa3ee8a2ce6b248122f28edfe56e4a69
TheZeOmlet <telmolopes1998@gmail.com>
2020-05-05 18:07:49 +0100

    Closes #315; test cases defined

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/discussion/service/StudentMakesAdditionalRequestTest.groovy
@@ -0,0 +1,28 @@
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class StudentMakesAdditionalRequestTest extends Specification {
+    def "student answers new question"() {
+
+    }
+    def "student answers new question by solved question"() {
+
+    }
+
+    def "question made by the same student that created the discussion"() {
+
+    }
+
+    @Unroll
+    def "make a new question with an empty message is #msg"() {
+
+    }
+
+    def "create a discussion with invalid user"() {
+
+    }
+
+    def "create a discussion with invalid user type"() {
+
+    }
+}
